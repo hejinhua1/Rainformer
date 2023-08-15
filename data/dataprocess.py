f13ba557b8d6958512327b0e38fa879be6625757
hejinhua1 <2584944133@qq.com>
2023-08-15 17:32:49 +0800

    add Unet

--- data/dataprocess.py
+++ data/dataprocess.py
@@ -4,8 +4,10 @@ import pygrib
 import numpy as np
 import locale
 import logging
+import datetime
 import xarray as xr
 import sys
+from einops import rearrange, reduce, parse_shape
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 # 设置日志文件路径
@@ -19,18 +21,25 @@ output_path = u'E:/HJHCloud/Seafile/typhoon data/bigdata/data.npy'
 
 
 if __name__ == '__main__':
-    big_data = []
-    oneHourData = np.zeros((13, 6, 41, 61), dtype=np.float32)
-    oneHour_oneVari_Data = np.zeros((6, 41, 61), dtype=np.float32)
-    i, j = 0, 0
+
     varName = ['z', 'q', 't', 'u', 'v', 'w']
     # 遍历目录下的所有文件
     for year_path in os.listdir(directory_path):
+        year = int(year_path)
+        if year<2018:
+            continue
 
-        logging.info('#########################开始处理{}年的数据#########################'.format(year_path))
+        oneHourData = np.zeros((13, 6, 41, 61), dtype=np.float32)
+        oneHour_oneVari_Data = np.zeros((6, 41, 61), dtype=np.float32)
+        i, j = 0, 0
+        big_data = []
+        print('#########################开始处理{}年的数据#########################'.format(year_path))
         year_file_path = os.path.join(directory_path, year_path)
         for month_path in os.listdir(year_file_path):
-            logging.info('##################开始处理{}月的数据##################'.format(month_path[:2]))
+            month = int(month_path[:2])
+            if month < 10:
+                continue
+            print('##################开始处理{}月的数据##################'.format(month_path[:2]))
             if month_path.endswith(".grib2"):
                 # 打开 grib2 文件
                 grib2file = os.path.join(year_file_path+'/', month_path)
@@ -45,7 +54,10 @@ if __name__ == '__main__':
                     # dataDate = grb.dataDate
                     # dataTime = grb.dataTime
                     # julianDay = grb.julianDay
-                    # year, month, day, hour = grb.year, grb.month, grb.day, grb.hour
+                    year, month, day, hour = grb.year, grb.month, grb.day, grb.hour
+                    if date < datetime.datetime(2018, 10, 7, 4, 0, 0):
+                        continue
+
                     shortName = grb.shortName
                     missingValue = grb.missingValue
                     # average = grb.average
@@ -56,31 +68,34 @@ if __name__ == '__main__':
                     data = grb.values
 
                     if data.shape != (41, 61):
-                        logging.info('########{}变量{}数据维度与其他不一致########'.format(date, variable_name))
+                        print('########{}变量{}数据维度与其他不一致########'.format(date, variable_name))
                         continue
 
                     # TODO 用try except 处理
                     # 异常处理
                     exists_missing = np.isin(missingValue, data)
                     if exists_missing:
-                        logging.info('########{}变量{}缺失处理########'.format(date, variable_name))
+                        print('########{}变量{}缺失处理########'.format(date, variable_name))
                         data[data == missingValue] = np.nan
                         # 计算非缺失值的均值
                         mean_value = np.nanmean(data)
                         # 使用均值来填充缺失值
                         data[np.isnan(data)] = mean_value
                     # 修改数组元素
-                    i_vari = int((i-1) % 6)
-                    assert varName[i_vari] == shortName, '########{}变量{}数据顺序出错########'.format(date, variable_name)
-                    oneHour_oneVari_Data[i_vari] = data.astype(np.float32) # 增加同一气压层不同变量的数据
-                    if (i%6 == 0): # 增加同一气压层下所有变量的数据
-                        i_press = int((i/6 - 1)%13)
-                        oneHourData[i_press] = oneHour_oneVari_Data
-                    if (i % 78 == 0):  # 增加同一气压层下所有变量的数据
-                        big_data.append(oneHourData)
+                    # i_vari = int((i-1) % 6)
+                    # assert varName[i_vari] == shortName, '########{}变量{}数据顺序出错########'.format(date, variable_name)
+                    # oneHour_oneVari_Data[i_vari] = data.astype(np.float32) # 增加同一气压层不同变量的数据
+                    # if (i%6 == 0): # 增加同一气压层下所有变量的数据
+                    #     i_press = int((i/6 - 1)%13)
+                    #     oneHourData[i_press] = oneHour_oneVari_Data
+                    # if (i % 78 == 0):  # 增加同一气压层下所有变量的数据
+                    #     big_data.append(oneHourData)
 
                 # 关闭 grib2 文件
                 grbs.close()
-
-        np.save(output_path, np.array(big_data))
-    logging.info('########################################## 数据处理已完成 ##########################################')
+    #     output_path = u'F:/HJH/HJHcloud/Seafile/bigdata/data{}.npy'.format(year_path)
+    #     reshaped_data = rearrange(np.array(big_data), 't h w x y -> t w h x y')
+    #     np.save(output_path, reshaped_data)
+    #     print(
+    #         '######################### {}年数据处理已完成 #########################'.format(year_path))
+    # print('########################################## 数据处理已完成 ##########################################')
